fa025974fb42db0c90d0ad9b0ec7a9874e6106ec
vd18 <vandandave18@gmail.com>
2021-03-30 09:59:59 +0530

    upload files

---
+++ app.js
@@ -0,0 +1,56 @@
+
+var btntranslate = document.querySelector("#btn-translate");
+var texarea = document.querySelector("#tex-area");
+var outputbox = document.querySelector("#outputbox")
+
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverurl = "https://api.funtranslations.com/translate/dothraki.json"
+
+function translateurl(text){
+   return serverurl + "?" + "text=" + text
+}
+
+function handler(error){
+   console.log("errror occured " + error )
+   alert("something went wrong try again after some time")
+
+
+}
+function clickeventhandler(){
+
+   var inputText = texarea.value
+
+   fetch(translateurl(inputText))
+   .then(response => response.json())
+   .then(json => {
+      var translatedtext = json.contents.translated
+      outputbox.innerText = translatedtext
+      
+   })
+   .catch(handler)
+   };
+
+
+btntranslate.addEventListener("click", clickeventhandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
